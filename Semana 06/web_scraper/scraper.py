f4c210896b7c204df75aad53235f42211ccf5589
Guilherme Bauer-Negrini <negrini.guilherme@gmail.com>
2020-06-14 13:19:26 -0300

    Refatora a função get_data
    `get_data` foi desmembrada nas funções:
    * `format_seminar`: formata os dados raspados para ficarem compatíveis com os atributos do objeto Seminar
    * `log_seminar_data`: imprime os atributos do objeto seminário registrado.
    * `get_seminars`: retorna a lista de objetos da classe Seminar para cada tag <article> no HTML da página.

--- Semana 06/web_scraper/scraper.py
+++ Semana 06/web_scraper/scraper.py
@@ -7,6 +7,7 @@ from datetime import datetime
 import traceback
 import sys
 
+
 class WebScraper:
 
     def __init__(self):
@@ -14,99 +15,92 @@ class WebScraper:
 
     def get_html(self, url):
         """Gets the HTML content from a given url"""
-        
+
         print("Getting HTML...")
         try:
             html = urlopen(url)
             print("HTML from: <" + url + "> retrieved.")
             return html
         except (HTTPError, URLError) as error:
-            print("Error while handling HTML retrieval. Check URL and internet connection.")
+            print(
+                "Error while handling HTML retrieval. Check URL and internet connection.")
             exc_type, exc_value, exc_tb = sys.exc_info()
             print(str(traceback.format_exception(exc_type, exc_value, exc_tb)))
 
     def parse_html(self, html):
         """Parses the HTML content with BeautifulSoup."""
-        
+
         print("Parsing HTML...")
         self.soup = bs4.BeautifulSoup(html.read(), "html.parser")
         print("HTML parsed.")
 
-       
+    def format_seminar(self, article):
+        """Formats the scraped data from HTML article tag into the attributes of a Seminar object."""
 
-    def get_data(self):
-        """Does all the magic to extract the seminars information. Returns a list of Seminar objects"""
+        seminar = Seminar()
         try:
-            # Gets all <articles> tag
-            articles_list = self.soup.find_all("article")
+            seminar.summary = " ".join(article.h2.getText().split()[0:-3])
+            seminar.description = article.p.getText() \
+                + "\nLink: <" + article.a.get("href") + ">"
+            seminar.link = "<" + article.a.get("href") + ">"
 
-            seminars = []
+            hour_regex = re.compile(r'.\dh')
+            minutes_regex = re.compile(r'\d.min')
+            date_regex = re.compile(r'.\d/.\d')
 
-            for article in articles_list:
+            day, month = date_regex.search(
+                article.h2.getText()).group().split("/")
+            day = int(day)
+            if (day < 10):  # If day has only one digit
+                # then a zero must be added to match the dateTime template
+                day = "0"+str(day)
+            day = str(day)
 
-                try:
-                    seminar_event = Seminar()
-                    seminar_event.summary = " ".join(article.h2.getText().split()[0:-3])
-                    seminar_event.description = article.p.getText() \
-                                                + "\nLink: <" + article.a.get("href") +">"
-                    seminar_event.link = "<" + article.a.get("href") +">"
+            hour = hour_regex.search(article.h2.getText()).group().split("h")[
+                0].replace(" ", "")
+            hour = int(hour)
+            if (hour < 10):    # If the hour has only one digit
+                # then a zero must be added to match the dateTime template
+                hour = "0"+str(hour)
+            hour = str(hour)
 
-                    # Regex to get hour, minutes and date from the article tag text
-                    hour_regex = re.compile(r'.\dh')
-                    minutes_regex = re.compile(r'\d.min')
-                    date_regex = re.compile(r'.\d/.\d')
+            try:
+                minutes = minutes_regex.search(
+                    article.h2.text).group().split("min")[0]
+            except AttributeError:
+                minutes = "00"      # Sometimes the minutes digits are absent so define it as "00"
 
-                    # Gets the text matching the date regex and splits into day and month
-                    day, month = date_regex.search(article.h2.getText()).group().split("/")
-                    day = int(day)
-                    if (day < 10): # If day has only one digit
-                        day = "0"+str(day)  # then a zero must be added to match the dateTime template
-                    day = str(day)
-
-                    # Gets the hours matching the hour regex, splits into the "h" delimiter and removes white space
-                    hour = hour_regex.search(article.h2.getText()).group().split("h")[0].replace(" ", "")
-
-                    hour = int(hour)
-                    if (hour < 10):    # If the hour has only one digit
-                        hour = "0"+str(hour)     # then a zero must be added to match the dateTime template
-
-                    hour = str(hour)
-
-                    try:
-                        # Gets the minutes matching the minutes regex, splits into the "min" delimiter
-                        minutes = minutes_regex.search(article.h2.text).group().split("min")[0]
-                    except AttributeError:
-                        minutes = "00"      # Sometimes the minutes digits are absent so define it as "00"
-
-                    # dateTime template : YYYY-MM-DDTHH:MM:SS-02:00
-                    seminar_event.start = str(datetime.today().year)+"-"+month+"-"+day+"T"\
-                                            +hour+":"+minutes+":"+"00"+"-03:00"
-                    end_hour = str(int(hour)+1)  # Seminars usually last one hour
-                    seminar_event.end = str(datetime.today().year)+"-"+month+"-"+day+"T"\
-                                            +end_hour+":"+minutes+":"+"00"+"-03:00"
-
-                    # Adds the seminar object to the seminars list
-                    if seminar_event is not None:
-                        seminars.append(seminar_event)
-
-                    # Logs the Seminar objects
-                    print("The following seminar was added to the list: ")
-                    for parameter in seminar_event.parameters():
-                        print(parameter)
-                except AttributeError:
-                    continue
-
-            # Returns the list of Seminar objects that will be used to create the Google Calendar events
-            return seminars
-
-        except Exception as error:
-            print("Error: " + error.__str__())
+            # dateTime template : YYYY-MM-DDTHH:MM:SS-03:00
+            seminar.start = str(datetime.today().year)+"-"+month+"-"+day+"T"\
+                + hour+":"+minutes+":"+"00"+"-03:00"
+            end_hour = str(int(hour)+1)  # Seminars usually last one hour
+            seminar.end = str(datetime.today().year)+"-"+month+"-"+day+"T"\
+                + end_hour+":"+minutes+":"+"00"+"-03:00"
+            return seminar
+        except AttributeError:
+            print(
+                "Error while formatting seminar data. Check HTML for missing tags.")
             exc_type, exc_value, exc_tb = sys.exc_info()
             print(str(traceback.format_exception(exc_type, exc_value, exc_tb)))
-    
-    def print_error_information(error){
-        """Prints the error message and"""
-        print("Error: " + error.__str__())
-            exc_type, exc_value, exc_tb = sys.exc_info()
-            print(str(traceback.format_exception(exc_type, exc_value, exc_tb)))
-    }
+            return None
+
+    def log_seminar_data(self, seminar):
+        """Prints Seminar object attributes."""
+
+        print("The following seminar was added to the list: ")
+        for parameter in seminar.parameters():
+            print(parameter)
+
+    def get_seminars(self):
+        """Returns a list of Seminar objects based on HTML article tags."""
+
+        articles = self.soup.find_all("article")
+        seminars = list()
+
+        for article in articles:
+            seminar = format_seminar(article)
+            if seminar is not None:
+                seminars.append(seminar)
+                log_seminar_data(seminar)
+
+        return seminars
