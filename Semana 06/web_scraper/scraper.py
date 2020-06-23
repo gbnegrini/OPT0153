97b5046a5741020ab6035ba679ee6c33ff35d8ed
Guilherme Bauer-Negrini <negrini.guilherme@gmail.com>
2020-06-23 11:35:01 -0300

    Extrai 4 novos métodos do método format_seminar
    Etapas individuais de formatação foram separadas em novos métodos:
    - format_text_data
    - format_date_data
    - format_time_data
    - set_datetime

--- Semana 06/web_scraper/scraper.py
+++ Semana 06/web_scraper/scraper.py
@@ -36,50 +36,63 @@ class WebScraper:
 
         seminar = Seminar()
         try:
-            seminar.summary = " ".join(article.h2.getText().split()[0:-3])
-            seminar.description = article.p.getText() \
-                + "\nLink: <" + article.a.get("href") + ">"
-            seminar.link = "<" + article.a.get("href") + ">"
-
-            hour_regex = re.compile(r'.\dh')
-            minutes_regex = re.compile(r'\d.min')
-            date_regex = re.compile(r'.\d/.\d')
-
-            day, month = date_regex.search(
-                article.h2.getText()).group().split("/")
-            day = int(day)
-            if (day < 10):  # If day has only one digit
-                # then a zero must be added to match the dateTime template
-                day = "0"+str(day)
-            day = str(day)
-
-            hour = hour_regex.search(article.h2.getText()).group().split("h")[
-                0].replace(" ", "")
-            hour = int(hour)
-            if (hour < 10):    # If the hour has only one digit
-                # then a zero must be added to match the dateTime template
-                hour = "0"+str(hour)
-            hour = str(hour)
-
-            try:
-                minutes = minutes_regex.search(
-                    article.h2.text).group().split("min")[0]
-            except AttributeError:
-                minutes = "00"      # Sometimes the minutes digits are absent so define it as "00"
-
-            # dateTime template : YYYY-MM-DDTHH:MM:SS-03:00
-            seminar.start = str(datetime.today().year)+"-"+month+"-"+day+"T"\
-                + hour+":"+minutes+":"+"00"+"-03:00"
-            end_hour = str(int(hour)+1)  # Seminars usually last one hour
-            seminar.end = str(datetime.today().year)+"-"+month+"-"+day+"T"\
-                + end_hour+":"+minutes+":"+"00"+"-03:00"
+            seminar = self.__format_text_data(article, seminar)
+            month, day = self.__format_date_data(article)
+            hour, minutes = self.__format_time_data(article)
+            seminar = self.__set_datetime(month, day, hour, minutes, seminar)
             return seminar
+
         except AttributeError:
             print(
                 "Error while formatting seminar data. Check HTML for missing tags.")
             exc_type, exc_value, exc_tb = sys.exc_info()
             print(str(traceback.format_exception(exc_type, exc_value, exc_tb)))
             return None
+
+    def __format_text_data(self, article, seminar):
+        seminar.summary = " ".join(article.h2.getText().split()[0:-3])
+        seminar.description = article.p.getText() \
+            + "\nLink: <" + article.a.get("href") + ">"
+        seminar.link = "<" + article.a.get("href") + ">"
+        return seminar
+
+    def __format_date_data(self, article):
+        date_regex = re.compile(r'.\d/.\d')
+
+        day, month = date_regex.search(
+            article.h2.getText()).group().split("/")
+        day = int(day)
+        if (day < 10):  # If day has only one digit
+            # then a zero must be added to match the dateTime template
+            day = "0"+str(day)
+        day = str(day)
+        return month, day
+
+    def __format_time_data(self, article):
+        hour_regex = re.compile(r'.\dh')
+        minutes_regex = re.compile(r'\d.min')
+        hour = hour_regex.search(article.h2.getText()).group().split("h")[
+            0].replace(" ", "")
+        hour = int(hour)
+        if (hour < 10):    # If the hour has only one digit
+            # then a zero must be added to match the dateTime template
+            hour = "0"+str(hour)
+        hour = str(hour)
+        try:
+            minutes = minutes_regex.search(
+                article.h2.text).group().split("min")[0]
+        except AttributeError:
+            minutes = "00"      # Sometimes the minutes digits are absent so define it as "00"
+        return hour, minutes
+
+    def __set_datetime(self, month, day, hour, minutes, seminar):
+        # dateTime template : YYYY-MM-DDTHH:MM:SS-03:00
+        seminar.start = str(datetime.today().year)+"-"+month+"-"+day+"T"\
+            + hour+":"+minutes+":"+"00"+"-03:00"
+        end_hour = str(int(hour)+1)  # Seminars usually last one hour
+        seminar.end = str(datetime.today().year)+"-"+month+"-"+day+"T"\
+            + end_hour+":"+minutes+":"+"00"+"-03:00"
+        return seminar
 
     def __log_seminar_data(self, seminar):
 
